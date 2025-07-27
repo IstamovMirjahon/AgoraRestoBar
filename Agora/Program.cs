7ad7abc86583956709a478d28abbb07f676408c3
OybekEshonqulov <oybekeshonqulov40@gmail.com>
2025-07-27 23:00:12 +0500

    update

--- Agora/Program.cs
+++ Agora/Program.cs
@@ -100,12 +100,11 @@ namespace Agora
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+        
                 app.MapOpenApi();
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
