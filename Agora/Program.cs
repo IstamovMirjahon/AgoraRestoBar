1ac5ea974c2b694f82fb3c3a849c5f8df51d750e
OybekEshonqulov <oybekeshonqulov40@gmail.com>
2025-07-30 18:03:35 +0500

    test

--- Agora/Program.cs
+++ Agora/Program.cs
@@ -20,10 +20,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
 });
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5043); // HTTP
-}); 
+builder.WebHost.UseUrls("http://0.0.0.0:5043", "https://0.0.0.0:7160");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
